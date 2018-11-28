65fa1661fdc12e2240b0fe6089ab83460bafc77d
lbc <2553486226@qq.com>
2018-11-28 20:15:33 +0800

    end

---
+++ src/main.js
@@ -0,0 +1,14 @@
+require("babel-runtime/regenerator")
+require("webpack-hot-middleware/client?reload=true")
+require("./main.css")
+require("./index.html")
+
+//alert("Hello World!!!")
+
+// var a =async () => {
+//     const { a, b} = args
+//     await console.log("Hello Future!",a,b)
+//     console.log("Done")
+// }
+
+// a({a:1,b:2})
